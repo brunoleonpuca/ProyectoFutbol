cb144cfc6ab94e9792fc1115e949aebdd502395b
brunoleonpuca <brunopuca1@gmail.com>
2021-08-19 18:48:01 -0300

    Browser driver setup

--- ProyectoFutbol/ProyectoFutbol/BrowserSetup/BrowserDriver.cs
+++ ProyectoFutbol/ProyectoFutbol/BrowserSetup/BrowserDriver.cs
@@ -5,8 +5,60 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
 
-    class BrowserDriver
+    public class BrowserDriver
     {
+        private IWebDriver driver;
+        private ChromeOptions options;
+        private Config configuration = new Config();
+
+        //This class is to download files:
+        //private DriverManager webDriverManager;
+
+        public IWebDriver SelectDefaultBrowser()
+        {
+            try
+            {
+                if (configuration.DefaultBrowser.Equals(DriverConsts.Chrome))
+                {
+                    //There must be a way to load Adblock with the Chrome
+                    options = new ChromeOptions();
+                    options.AddExtension("C:\\repos\\ChromeExtensions\\4.35.0_0.crx");
+                    driver = new ChromeDriver(options);
+                    driver.Manage().Window.Maximize();
+                }
+            }
+            catch
+            {
+                throw new Exception(DriverExceptionsConsts.BrowserNotValidException);
+            }
+
+            return driver;
+        }
+
+        public void KillDriver()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch
+            {
+                throw new Exception(DriverExceptionsConsts.KillDriverException);
+            }
+        }
+
+        public static class DriverConsts
+        {
+            public static string Chrome = "Chrome";
+        }
+
+        public static class DriverExceptionsConsts
+        {
+            public static string BrowserNotValidException = "The browser is not valid";
+            public static string KillDriverException = "Couldn't kill driver";
+        }
     }
 }
