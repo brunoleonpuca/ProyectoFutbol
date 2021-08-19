604cc50e9f70fcb042a07764aa917977b6fb211b
brunoleonpuca <brunopuca1@gmail.com>
2021-08-19 01:11:26 -0300

    I liked the design. i think

---
+++ ProyectoFutbol/ProyectoFutbol/Builders/LeaguesBuilder/LeagueElements.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoFutbol.Builders.LeaguesBuilder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class LeagueElements
+    {
+    }
+}
