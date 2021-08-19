501d6ee5da5bdd53eed55dcef16ee3d27f55f9cb
brunoleonpuca <brunopuca1@gmail.com>
2021-08-19 01:00:41 -0300

    Setup project

---
+++ ProyectoFutbol/ProyectoFutbol/Config.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyectoFutbol
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class Config
+	{
+		public string DefaultBrowser = ConfigurationManager.AppSettings["DefaultBrowser"];
+
+		public string TransferMrktURL = ConfigurationManager.AppSettings["TransferMrktURL"];
+
+	}
+}
